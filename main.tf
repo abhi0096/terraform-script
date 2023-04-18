61061b11e0bd4f2093ab0ff8f57f807534e47102
Abhishek Khandelwal <106994013+abhi0096@users.noreply.github.com>
2023-04-18 17:54:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
-# provider "aws" {
-#   region = "us-east-1"
-# }
+provider "aws" {
+  region = "us-east-1"
+}
 
 resource "aws_instance" "example" {
-  ami           = "ami-0aa2b7722dc1b5612" # Enter the AMI ID for your desired instance type
+  ami           = "ami-0c94855ba95c71c99" # Enter the AMI ID for your desired instance type
   instance_type = "t2.micro"
   
   # Enter your desired key pair name or remove this block if you don't want to use a key pair
-#  key_name      = "test-pem" 
+  key_name      = "my-keypair" 
 
   tags = {
     Name = "my-ec2-instance"
