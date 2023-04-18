1fd711ee7c3f1728ad9bac3ca917e38a1f3d56c8
Abhishek Khandelwal <106994013+abhi0096@users.noreply.github.com>
2023-04-18 17:42:34 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,10 +1,10 @@
 
-output "public_ip" {
- value       = aws_instance.my_vm.public_ip
- description = "Public IP Address of EC2 instance"
-}
+# output "public_ip" {
+#  value       = aws_instance.my_vm.public_ip
+#  description = "Public IP Address of EC2 instance"
+# }
 
-output "instance_id" {
- value       = aws_instance.my_vm.id
- description = "Instance ID"
-}
+# output "instance_id" {
+#  value       = aws_instance.my_vm.id
+#  description = "Instance ID"
+# }
