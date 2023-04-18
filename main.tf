0cffd36c82e50d085aaa556b93db154f6ab631b0
Abhishek Khandelwal <106994013+abhi0096@users.noreply.github.com>
2023-04-18 17:26:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,15 @@
-resource "aws_instance" "my_vm" {
- ami           = var.ami //Ubuntu AMI
- instance_type = var.instance_type
-
- tags = {
-   Name = var.name_tag,
- }
+provider "aws" {
+  region = "us-east-1"
 }
 
+resource "aws_instance" "example" {
+  ami           = "ami-0aa2b7722dc1b5612" # Enter the AMI ID for your desired instance type
+  instance_type = "t2.micro"
+  
+  # Enter your desired key pair name or remove this block if you don't want to use a key pair
+  key_name      = "test-pem" 
+
+  tags = {
+    Name = "my-ec2-instance"
+  }
+}
