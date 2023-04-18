5bad47b5c97b158e828365d0c1ac393fd42e2931
Abhishek Khandelwal <106994013+abhi0096@users.noreply.github.com>
2023-04-18 18:16:39 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
-terraform {
-  backend "s3" {
-    bucket         = "my-terraform-state-bucket"
-    key            = "my-terraform-state-key"
-    region         = "us-east-1"
-    dynamodb_table = "my-terraform-state-lock"
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket         = "my-terraform-state-bucket"
+#     key            = "my-terraform-state-key"
+#     region         = "us-east-1"
+#     dynamodb_table = "my-terraform-state-lock"
+#   }
+# }
