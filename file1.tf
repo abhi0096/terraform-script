8cf4228991c75667a58eebd343dabbd57e30db5c
Abhishek Khandelwal <106994013+abhi0096@users.noreply.github.com>
2023-04-19 15:18:37 +0530

    Update file1.tf

--- file1.tf
+++ file1.tf
@@ -1,23 +1,57 @@
-terraform {
-        required_providers {
-        aws = {
-                source = "hashicorp/aws"
-                version = "~> 4.0"
-}
-}
-}
-
 provider "aws" {
-        region = "us-east-1"
-        access_key = "AKIAT5QV5NVCZAEFT47S"
-        secret_key = "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"
+  region = "us-east-1"
 }
 
-resource "aws_instance" "s1" {
-  ami = "ami-0aa2b7722dc1b5612"
-  instance_type = "t2.micro"
+resource "aws_security_group" "example" {
+  name_prefix = "example-"
+  ingress {
+    from_port   = 80
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
-        tags = {
-           Name = "server1"
-}
-}
+#resource "aws_security_group_rule" "example" {
+#  type        = "ingress"
+#  from_port   = 80
+#  to_port     = 443
+#  protocol    = "tcp"
+#  cidr_blocks = ["0.0.0.0/0"]
+#  security_group_id = aws_security_group.example.id
+#}
+
+
+
+
+
+
+
+
+
+
+
+
+# terraform {
+#         required_providers {
+#         aws = {
+#                 source = "hashicorp/aws"
+#                 version = "~> 4.0"
+# }
+# }
+# }
+
+# provider "aws" {
+#         region = "us-east-1"
+#         access_key = "AKIAT5QV5NVCZAEFT47S"
+#         secret_key = "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"
+# }
+
+# resource "aws_instance" "s1" {
+#   ami = "ami-0aa2b7722dc1b5612"
+#   instance_type = "t2.micro"
+
+#         tags = {
+#            Name = "server1"
+# }
+# }
