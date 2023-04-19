293e757addf3df389e4ad3e84a89cf7916929010
Abhishek Khandelwal <106994013+abhi0096@users.noreply.github.com>
2023-04-19 12:44:02 +0530

    Delete backend.tf

--- backend.tf
+++
@@ -1,8 +0,0 @@
-# terraform {
-#   backend "s3" {
-#     bucket         = "my-terraform-state-bucket"
-#     key            = "my-terraform-state-key"
-#     region         = "us-east-1"
-#     dynamodb_table = "my-terraform-state-lock"
-#   }
-# }
